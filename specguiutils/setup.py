e1269dce995fd0f7bb1dffc223aaa5301d017d41
John Hammonds <JPHammonds@anl.gov>
2017-05-16 11:25:13 -0500

    Check in new specguiutils

---
+++ specguiutils/setup.py
@@ -0,0 +1,23 @@
+'''
+ Copyright (c) 2017, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+from setuptools import setup
+
+
+setup(name='specguiutils',
+      version='0.0.1',
+      description='Library to provide PyQt4 widgets to display spec file information read using ' +
+                   'spec2nexus.spec file library',
+      author = 'John Hammonds',
+      author_email = 'JPHammonds@anl.gov',
+      url = '',
+      packages = ['specguiutils',
+                  'specguiutils.scanbrowser',
+                  'specguiutils.counterselector'] ,
+      install_requires = ['spec2nexus', ],
+      license = 'See LICENSE File',
+      platforms = 'any',
+#       scripts = ['Scripts/rsMap3D',
+#                  'Scripts/rsMap3D.bat'],
+)
