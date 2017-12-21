fd83312cf6a1c5049198fc6898aa1391bb5224fb
John Hammonds <JPHammonds@anl.gov>
2017-12-21 14:07:09 -0600

    Code cleanup. Add documentation comments to class and method to address Issue #1

--- specguiutils/specguiutils/scantypeselector.py
+++ specguiutils/specguiutils/scantypeselector.py
@@ -12,7 +12,9 @@ SCAN_TYPES = ['All',]
 class ScanTypeSelector(qtWidgets.QDialog):
     '''
     Provide a ComboBox to allow switching scan between scan types included 
-    in the spec file.  An additional item 
+    in the spec file.  An additional item is added at the top to allow selecting 
+    all types.  This widget was developled to work along with the 
+    :py:class:`.ScanBrowser`
     '''
     scanTypeChanged = qtCore.pyqtSignal(int, name='scanTypeChanged')
     
@@ -36,6 +38,10 @@ class ScanTypeSelector(qtWidgets.QDialog):
 
             
     def loadScans(self, scanTypes):
+        '''
+        Load this module with a list scan types discovered in the spec these 
+        items, along with \'All\' will be added to the comboBox options
+        '''
         self.scanTypeSelection.currentIndexChanged[int] \
             .disconnect(self.typeSelectionChanged)
         self.scanTypeSelection.clear()
@@ -51,6 +57,9 @@ class ScanTypeSelector(qtWidgets.QDialog):
             .connect(self.typeSelectionChanged)
          
     def getTypeNames(self):
+        '''
+        return a list of types from the widget.
+        '''
         typeNames = []
         nameCount = self.scanTypeSelection.count()
         for index in range(nameCount):
@@ -58,18 +67,32 @@ class ScanTypeSelector(qtWidgets.QDialog):
         return typeNames
      
     def getTypeIndexFromName(self, typeName):
+        '''
+        Return an index of for a particular type name
+        '''
         names = self.getTypeNames()
         index = names.index(typeName)
         return index
          
     def getCurrentType(self):
+        '''
+        return the currently selected type
+        '''
         return str(self.scanTypeSelection.currentText())
     
     def setCurrentType(self, newType):
+        '''
+        Set the current type programatically
+        '''
         self.scanTypeSelection.setCurrentIndex(newType)
          
     @qtCore.pyqtSlot(int)
     def typeSelectionChanged(self, newType):
+        '''
+        Send a signal to the rest of the application that the selected type has 
+        changed.  Applications that want to be notified should look for the 
+        signal appSelectorName.scanTypeChanged.connect(appHandlerMethod)
+        '''
 #        self.scanTypeSelection.setCurrentIndex(newType)
         logger.debug("Enter")
         self.scanTypeChanged[int].emit(newType)
